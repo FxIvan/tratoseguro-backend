33c6dec910deb5bffaef26340023c6c791925905
ialmendrasafe <almendraivan210814@gmail.com>
2023-10-14 13:26:07 -0300

    agregando insertOne de mongodb

---
+++ domain/signup.go
@@ -0,0 +1,6 @@
+package domain
+
+type SignupRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
--- routes/signupRouter.go
+++ routes/signupRouter.go
@@ -1,27 +1,43 @@
 package route
 
 import (
-	"fmt"
+	"context"
 	"time"
 
 	"github.com/FxIvan/bootstrap"
+	"github.com/FxIvan/domain"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func SignUp(env *bootstrap.Env, timeout time.Duration, db mongo.Database, group *gin.RouterGroup) {
 	group.POST("/signup", func(c *gin.Context) {
-		var request struct {
-			Email    string `json:"email"`
-			Password string `json:"password"`
-		}
+		var request domain.SignupRequest
 
-		if err := c.ShouldBindJSON(&request); err != nil {
+		err := c.ShouldBind(&request)
+		if err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
 			return
 		}
-		fmt.Println(request)
-		c.JSON(200, gin.H{"message": "ok"})
+
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		user := domain.SignupRequest{
+			Email:    request.Email,
+			Password: request.Password,
+		}
+
+		collection := db.Collection("users")
+
+		_, err = collection.InsertOne(ctx, user)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(200, gin.H{"message": "User created successfully"})
+
 	})
 
 }
