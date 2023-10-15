a5a378541ba2fbbed0a1984ffc0a00d91c6e6b76
ialmendrasafe <almendraivan210814@gmail.com>
2023-10-15 10:59:20 -0300

    encryptado

--- domain/signup.go
+++ domain/signup.go
@@ -4,3 +4,8 @@ type SignupRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type SignupRequestEncrypt struct {
+	Email    []byte `json:"email"`
+	Password []byte `json:"password"`
+}
--- routes/signupRouter.go
+++ routes/signupRouter.go
@@ -2,7 +2,14 @@ package route
 
 import (
 	"context"
+	"fmt"
 	"time"
+
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
 
 	"github.com/FxIvan/bootstrap"
 	"github.com/FxIvan/domain"
@@ -23,9 +30,40 @@ func SignUp(env *bootstrap.Env, timeout time.Duration, db mongo.Database, group 
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
 
-		user := domain.SignupRequest{
-			Email:    request.Email,
-			Password: request.Password,
+		key, _ := hex.DecodeString("6368616e676520746869732070617373776f726420746f206120736563726574")
+		plain_password := []byte(request.Password)
+		plain_email := []byte(request.Email)
+
+
+		block, err := aes.NewCipher(key)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		nonce := make([]byte, 12)
+
+
+		if _,err := io.ReadFull(rand.Reader, nonce); err != nil {
+			fmt.Println("ERROR ->",err.Error())
+			panic(err.Error())
+		}
+
+		aesgcm, err := cipher.NewGCM(block)
+		if err != nil {
+			fmt.Println(err.Error())
+			panic(err.Error())
+		}
+
+		ciphertext := aesgcm.Seal(nil, nonce, plain_password, nil)
+		ciphertext2 := aesgcm.Seal(nil, nonce, plain_email, nil)
+
+		fmt.Printf("%x\n", ciphertext)
+		fmt.Printf("%x\n", ciphertext2)
+
+		user := domain.SignupRequestEncrypt{
+			Email:    ciphertext,
+			Password: ciphertext2,
 		}
 
 		collection := db.Collection("users")
